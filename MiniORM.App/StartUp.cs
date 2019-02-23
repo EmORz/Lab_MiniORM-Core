2f49ece0644213a77538624cd01c7b3414d223e1
Emo <eiv.razgrad@gmail.com>
2019-02-23 23:25:45 +0200

    Add

---
+++ MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var conectionString = @"Server=DESKTOP-CP2NEHV\SQLEXPRESS;Database=MiniORM; Integrated Security=True";
+
+            var context = new SoftUniDbContext(conectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName =  "Gosho",
+                LastName = "Inserted",
+                DepartmentId =  context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
